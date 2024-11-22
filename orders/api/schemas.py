caadce876da1a57cdbdcfcec14648dae83ba80fc
mykhailohostishchev <mykhailo.hostishchev@intellias.com>
2024-11-23 00:57:28 +0200

    adding default api functional

---
+++ orders/api/schemas.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
+from uuid import UUID
+
+from pydantic import BaseModel, conint, validator, conlist
+
+
+class Size(Enum):
+    small = "small"
+    medium = "medium"
+    big = "big"
+
+
+class StatusEnum(Enum):
+    created = "created"
+    paid = "paid"
+    progress = "progress"
+    cancelled = "cancelled"
+    dispatched = "dispatched"
+    delivered = "delivered"
+
+
+class OrderItemSchema(BaseModel):
+    product: str
+    size: Size
+    quantity: Optional[conint(ge=1, strict=True)] = 1
+
+
+class CreateOrderSchema(BaseModel):
+    order: conlist(OrderItemSchema)
+
+
+class GetOrderSchema(CreateOrderSchema):
+    id: UUID
+    created: datetime
+    status: StatusEnum
+
+
+class GetOrdersSchema(BaseModel):
+    orders: List[GetOrderSchema]
